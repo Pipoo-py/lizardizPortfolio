92b4e486b07b8e2240baef30bf03b5f15889f402
Alejandro Salazar Lizardiz <asalazarlizardiz@gmail.com>
2025-04-12 16:38:49 -0400

    add assetPrefix

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
-const repoName = 'lizardizPortfolio';
-
 export default defineConfig({
   plugins: [pluginReact()],
   output:{
-     assetPrefix: `/${repoName}/`, 
+     assetPrefix: "/lizardizPortfolio/", 
   }
 });
