db04fbd55dfdc1a892847ebb9323899d42b124b7
Alejandro Salazar Lizardiz <asalazarlizardiz@gmail.com>
2025-04-13 16:26:19 -0400

    html.title config

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -3,6 +3,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html:{
+    title: "Alejandro Lizardiz | Desarrollador Frontend React"
+  },
   output:{
      assetPrefix: "/lizardizPortfolio/", 
   }
